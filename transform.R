f50d9fd634763d73ff45d9cabc7c0e12b234263c
LeonardoFlores1 <leonardoflores150@gmail.com>
2020-05-07 09:49:56 -0600

    modificando

--- transform.R
+++ transform.R
@@ -15,7 +15,7 @@ prop.table(table(survey$practica_actualmente))*100 #muestra porcentajes de las f
 
 as.data.frame(prop.table(table(survey$practica_actualmente))) # devuelve la info en forma de columna
 
-df_perc_practica_actualmente <- as.data.frame(prop.table(table(survey$practica_actualmente)))#asignamos el resultado de la linea anterior a la variable df
+df_perc_practica_actualmente <- as.data.frame(prop.table(table(survey$practica_actualmente))*100)#asignamos el resultado de la linea anterior a la variable df
 
 library(dplyr) #carga la ibreria 
 
@@ -36,13 +36,15 @@ prop.table(table(survey$practica_primera_opcion))
 
 as.data.frame(prop.table(table(survey$practica_primera_opcion)))
 
-df_perc_primera_opcion <- as.data.frame(prop.table(table(survey$practica_primera_opcion)))
+df_perc_primera_opcion <- as.data.frame(prop.table(table(survey$practica_primera_opcion))*100)
 
 df_perc_primera_opcion <- df_perc_primera_opcion %>% arrange(-Freq)
 
 boxplot(df_perc_primera_opcion$Freq)
 
 hist(df_perc_primera_opcion$Freq)
+
+qqnorm(df_perc_primera_opcion$Freq)
 #-------------------------------------------columna practica cursando clases--------------------------------------------
 survey$practica_cursando_clases <- as.factor(survey$practica_cursando_clases)
 
@@ -52,16 +54,51 @@ prop.table(table(survey$practica_cursando_clases))
 
 as.data.frame(prop.table(table(survey$practica_cursando_clases)))
 
-df_perc_cursa_clases <- as.data.frame(prop.table(table(survey$practica_cursando_clases)))
+df_perc_cursa_clases <- as.data.frame(prop.table(table(survey$practica_cursando_clases))*100)
 
 df_perc_cursa_clases <- df_perc_cursa_clases %>% arrange(-Freq)
 
 boxplot(df_perc_cursa_clases$Freq)
 
 hist(df_perc_cursa_clases$Freq)
-#----------------------------------------------columna Rango indice-----------------------------------------------
 
+qqnorm(df_perc_cursa_clases$Freq)
+#----------------------------------------------columna Rango indice-----------------------------------------------
+survey$rango_indice <- as.factor(survey$rango_indice)
+
+table(survey$rango_indice)
+
+prop.table(table(survey$rango_indice))
+
+as.data.frame(prop.table(table(survey$rango_indice)))
+
+df_perc_rango_indice <- as.data.frame(prop.table(table(survey$rango_indice))*100)
+
+df_perc_rango_indice <- df_perc_rango_indice %>% arrange(-Freq)
+
+boxplot(df_perc_rango_indice$Freq)
+
+hist(df_perc_rango_indice$Freq)
+
+qqnorm(df_perc_rango_indice$Freq)
 #----------------------------------------------columna rendimiento academico-------------------------------------
+survey$rendimiento_academico <- as.factor(survey$rendimiento_academico) 
+
+table(survey$rendimiento_academico)
+
+prop.table(table(survey$rendimiento_academico))
+
+as.data.frame(prop.table(table(survey$rendimiento_academico)))
+
+df_perc_rendimiento_academico <- as.data.frame(prop.table(table(survey$rendimiento_academico))*100)
+
+df_perc_rendimiento_academico <- df_perc_rendimiento_academico %>% arrange(-Freq)
+
+boxplot(df_perc_rendimiento_academico$Freq)
+
+hist(df_perc_rendimiento_academico$Freq)
+
+qqnorm(df_perc_rendimiento_academico$Freq)
 
 #------------------------------------------------------------------------------------------------------------
 
